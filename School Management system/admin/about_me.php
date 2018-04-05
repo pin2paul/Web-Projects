cf5f9b5fcfa46f4131742e30c5a763f4c3b2f3e1
Pintu Chandra Paul <pintu0167@gmail.com>
2018-04-05 15:45:39 +0600

    Create about_me.php

---
+++ School Management system/admin/about_me.php
@@ -0,0 +1,38 @@
+<?php	
+	session_start();
+	if($_SESSION['authuser'] != 1){
+		header('location: ../login.php');
+		exit();
+	}
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Bijoypur High School</title>
+	<link rel="stylesheet" href="../style.css" />
+	<link rel="stylesheet" href="../jQuery/jquery-ui.min.css" />
+	<script src="../jQuery/jquery.js" ></script>
+	<script src="../jQuery/jquery-ui.js" ></script>
+	<script src="../js/script.js" ></script>
+	<link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+	<section class="main_body">
+		<header class="header">
+			<?php include ('header.php')?>
+		</header>
+		<section style="margin-top:100px;">
+			<table class="teacher_details" width="900" height="150" border="2px" >
+				<tr>
+				<td align="center" width="60%"><span style="color:#b92929;font-size:200%;line-height:50px">Pintu Chandra Paul</span><br><span style="color:#00b900;font-size:180%;line-height:40px">Dept. of ICT</span><br><span style="color:#3434A2;font-size:160%;line-height:30px">Session: 2012-13</span><br><span  style="color:#00a898;font-size:140%;line-height:20px">Comilla University</span><br><span style="font-size:120%;">Mobile: +8801816103470</span><br><span style="font-size:120%;">E-mail: pintu.ict@gmail.com</span></td>
+				<td align="center"><img src="../images/pintu.jpg" alt="Photo" width="300" height="300"/></td></tr>
+			</table>
+		</section>
+		<div style="clear:both;margin-top:25px"></div>
+		<?php include('footer.php'); ?>
+		<script src="../js/jquery-1.9.1.js"></script> 
+	</section>
+</body>
+</html>
